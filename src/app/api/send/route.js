3287bd9f56d0ffedceafb622559da7fe38aa4e12
leonardo mota <lmota5780@gmail.com>
2025-01-04 20:27:04 -0400

    cambios y correcciones en routes.js

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,27 +1,28 @@
-// import { EmailTemplate } from '../../../components/EmailTemplate';
-// import { Resend } from "resend";
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
 
-// const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
 
-// export async function POST() {
-//   try {
-//     const { data, error } = await resend.emails.send({
-//       from: "Leonardo <lmota5780@gmail.com>",
-//       to: ["lmota5780@gmail.com"],
-//       subject: "Hello world",
-//       react: (
-//         <>
-//           <p>Email Body</p>
-//         </>
-//       ),
-//     });
-
-//     if (error) {
-//       return Response.json({ error }, { status: 500 });
-//     }
-
-//     return Response.json(data);
-//   } catch (error) {
-//     return Response.json({ error }, { status: 500 });
-//   }
-// }
+export async function POST(req, res) {
+  const { email, subject, message } = await req.json();
+  console.log(email, subject, message);
+  try {
+    const data = await resend.emails.send({
+      from: fromEmail,
+      to: [fromEmail, email],
+      subject: subject,
+      react: (
+        <>
+          <h1>{subject}</h1>
+          <p>Thank you for contacting us!</p>
+          <p>New message submitted:</p>
+          <p>{message}</p>
+        </>
+      ),
+    });
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
